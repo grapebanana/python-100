dbce71b976463ea0d55a4e323ba95fde0b8eba3a
grapebanana <128008463+grapebanana@users.noreply.github.com>
2023-03-23 12:55:12 +0900

    Add files via upload

---
+++ 파이썬 기초.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+print("KOREA POLYTECHNICS") #따라하기1
+print()
+print("한국 폴리텍대학")
+
+
+# In[6]:
+
+
+print("KOEREA POLYTECHNICS은 "한국폴리텍 대학"입니다.") #따라하기2
+print('KOREA POLYTECHNICS은 '한국폴리텍 대학' 입니다.')
+
+
+# In[7]:
+
+
+print("KOREA POLYTECHNICS은 '한국폴리텍 대학'입니다.") #따라하기3
+print('KOREA POLYTECHNICS은 "한국폴리텍 대학"입니다.')
+
+
+# In[8]:
+
+
+print(7+7) #따라하기4
+print(7-7)
+print(7*7)
+print(7/7)
+
+
+# In[ ]:
+
+
+a= input("첫 번째 숫자를 입력하세요.")  #따라하기5
+b= input("두 번째 숫자를 입력하세요.") 
+a= int(a)
+b= int(b)
+
+print(a+b,a-b, a*b, a/b)
+
+
+# In[ ]:
+
+
+print(파이썬은 처음 배웁니다.)   #스스로하기1
+print(1 2 3 4 5 )
+print(100)
+
+
+# In[ ]:
+
+
+print("KOREA POLYTECHNICS", '한국폴리텍대학', 100) #스스로하기2
+
+
+# In[ ]:
+
+
+a=1    #스스로하기3
+a=a+1
+a=
+
+
+# In[ ]:
+
+
+a,b= 10,20 #스스로하기4
+print(a,b)
+a,b=b,a
+print(a,b)
+
+
+# In[ ]:
+
+
+#스스로하기5
+
+
+# In[ ]:
+
+
+a= int(10.5)     #스스로 하기6
+b= float(2.17)
+c= complex(5,2)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
